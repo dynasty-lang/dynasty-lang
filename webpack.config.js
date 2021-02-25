b9d8999bd9c71d843de571e05c0300734a2f2037
Yusei Ito <me@yuseiito.com>
2021-02-25 18:49:31 +0900

    出力先の指定

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
 module.exports = {
  mode: 'development',
  entry: './src/index.ts',
  target: 'node',
+  output: {
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'index.js'
+  },
  module: {
    rules: [
      {
