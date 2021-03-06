e517bf1a62a8e9c3d2dd4972ed5c3e7f75e73b6e
frodo821 <sakaic2003@gmail.com>
2021-03-06 13:22:27 +0900

    AstNodeの型を整理

--- src/ast/astNode.ts
+++ src/ast/astNode.ts
@@ -1,83 +1,46 @@
-export type NodeKind =
-  | 'dnkTop'
-  | 'dnkIdent'
-  | 'dnkBlock'
-  | 'dnkBlockExpr'
-  | 'dnkFor'
-  | 'dnkWhile'
-  | 'dnkTypeDecl'
-  | 'dnkTypeDesc'
-  | 'dnkTypeLit'
-  | 'dnkMemberItem'
-  | 'dnkArrayTypeLit'
-  | 'dnkImport'
-  | 'dnkImportFrom'
-  | 'dnkImportList'
-  | 'dnkImportName'
-  | 'dnkFnDecl'
-  | 'dnkIfExpr'
-  | 'dnkVarDecl'
-  | 'dnkConstDecl'
-  | 'dnkParamList'
-  | 'dnkParamItem'
-  | 'dnkReturn'
-  | 'dnkBreak'
-  | 'dnkContinue'
-  | 'dnkCallExpr'
-  | 'dnkArgList'
-  | 'dnkNamedArg'
-  | 'dnkNamedArgList'
-  | 'dnkFloatLit'
-  | 'dnkIntLit'
-  | 'dnkStrLit'
-  | 'dnkObjectLit'
-  | 'dnkArrayLit'
-  | 'dnkOperations'
-  | 'dnkMemberAccessOp'
-  | 'dnkArrayAccessOp'
-  | 'dnkAssignOp'
-  | 'dnkPar'
-  | 'dnkFqn'
-  | 'dnkStmt'
-  | 'dnkEmpty';
+import { AstNode, DnkEmpty, unaryOperators } from './nodeKind';
 
-export const unaryOperators = { not: 1 };
-export const biaryOperators = {
-  '**': 7,
-  '*': 6,
-  '/': 6,
-  mod: 6,
-  '+': 5,
-  '-': 5,
-  '&': 4,
-  '|': 4,
-  '^': 4,
-  '<<': 3,
-  '>>': 3,
-  '>': 2,
-  '>=': 2,
-  '==': 2,
-  '!=': 2,
-  '<=': 2,
-  '<': 2,
-  and: 1,
-  or: 1,
-  xor: 1,
-  '=': 0,
-};
+export { assignOperators, biaryOperators, unaryOperators } from './nodeKind';
+export type {
+  AssignOperators,
+  AstNode,
+  BiaryOperators,
+  DnkArgList,
+  DnkArrayAccessOp,
+  DnkArrayLit,
+  DnkArrayTypeLit,
+  DnkBlockExpr,
+  DnkCallExpr,
+  DnkEmpty,
+  DnkFloatLit,
+  DnkFqn,
+  DnkIdent,
+  DnkIfExpr,
+  DnkImportList,
+  DnkImportName,
+  DnkIntLit,
+  DnkMemberAccessOp,
+  DnkMemberItem,
+  DnkNamedArg,
+  DnkObjectLit,
+  DnkObjectLitMember,
+  DnkOperations,
+  DnkPar,
+  DnkParamItem,
+  DnkParamList,
+  DnkStrLit,
+  DnkTypeLit,
+  ExpressionNode,
+  Operations,
+  TypeDescriptorNode,
+  UnaryOperators,
+} from './nodeKind';
 
-export type UnaryOperators = keyof typeof unaryOperators;
-export type BiaryOperators = keyof typeof biaryOperators;
-export type Operations = BiaryOperators | UnaryOperators;
+export type NodeKind = AstNode['kind'];
+
 export type NodeValue = AstNode | string | number | undefined;
 
-export interface AstNode {
-  kind: NodeKind;
-  value?: NodeValue | NodeValue[];
-  children: AstNode[];
-}
-
-export const dnkEmpty: AstNode = { kind: 'dnkEmpty', children: [] };
+export const dnkEmpty: DnkEmpty = { kind: 'dnkEmpty', children: [] };
 
 export function dumpNodeValue(value: NodeValue | NodeValue[]): string {
   if (value instanceof Array) {
@@ -103,15 +66,16 @@ export function dumpNodeValue(value: NodeValue | NodeValue[]): string {
       case 'dnkFloatLit':
         return (node.value as number).toString();
       case 'dnkStrLit':
-        return JSON.stringify(node.value as string);
+        return `${node.value[1] == 'r' ? 'r' : ''}"${node.value[1]}"`;
       case 'dnkArrayAccessOp':
         return `${dumpNodeValue(node.children[0])}[${dumpNodeValue(
           node.children[1]
         )}]`;
       case 'dnkArrayTypeLit':
-        return `${dumpNodeValue(
-          node.children[0]
-        )}${(node.value as AstNode[]).map((it) => `[${dumpNodeValue(it)}]`)}`;
+        return `${dumpNodeValue(node.children[0])}${(node.value instanceof Array
+          ? node.value
+          : []
+        ).map((it: any) => `[${dumpNodeValue(it)}]`)}`;
       case 'dnkParamList':
         return `(${node.children.map(dumpNodeValue).join()})`;
       case 'dnkParamItem':
@@ -138,11 +102,11 @@ export function dumpNodeValue(value: NodeValue | NodeValue[]): string {
 
 export function dumpAst(node: AstNode, depth = 0): void {
   let details = '';
-  if (node.value) {
+  if ('value' in node) {
     details = dumpNodeValue(node.value);
   }
   console.log(`${'  '.repeat(depth)}${node.kind} ${details || ''}`);
-  node.children.forEach((it) => {
+  node.children.forEach((it: any) => {
     dumpAst(it, depth + 1);
   });
 }
