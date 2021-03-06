de7f5039112d6407cd2f644677eab9f06f2f4a2c
frodo821 <sakaic2003@gmail.com>
2021-03-06 13:23:47 +0900

    AstNodeVisitorをinterfaceからabstract classに変更

--- src/compile/astNodeVisitor.ts
+++ src/compile/astNodeVisitor.ts
@@ -1,5 +1,12 @@
 import { AstNode } from '../ast/astNode';
 
-export interface AstNodeVisitor<T> {
-  visit(node: AstNode): T;
+export abstract class AstNodeVisitor<T> {
+  abstract visit(node: AstNode): T;
+  protected abstract canVisitThis(node: AstNode): boolean;
+
+  protected visitChildren(nodes: AstNode[]): T[] {
+    return nodes
+      .filter((it) => this.canVisitThis(it))
+      .map((it) => this.visit(it));
+  }
 }
