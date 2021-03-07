1118ac8e2ca5435768f57a6d1b10d0c9857170e5
frodo821 <sakaic2003@gmail.com>
2021-03-07 11:05:39 +0900

    compile引数を追加

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import parseArgs from 'command-line-args';
 import { dumpAst } from './ast/astNode';
-import { getTokenStream, parse, generateAst } from './parsing';
+import { getTokenStream, parse, generateAst, compile } from './parsing';
 
 function entry() {
   const args = parseArgs([
@@ -11,11 +11,30 @@ function entry() {
       multiple: true,
       type: String,
     },
-  ]);
+    {
+      name: 'compile',
+      alias: 'c',
+      type: Boolean,
+    },
+  ] as parseArgs.OptionDefinition[]);
   const inputFiles: string[] = args['input'];
   if (!inputFiles) {
     return;
   }
+
+  if (args.compile) {
+    inputFiles.forEach((it) => {
+      const content = fs.readFileSync(it, { encoding: 'utf-8' });
+      const tokenStream = getTokenStream(content);
+      const tree = parse(tokenStream);
+      const ast = generateAst(tree);
+      const path = it.replace(/\\\\/g, '/').split(/\//g);
+      const code = compile(ast, path[path.length - 1]);
+      console.log(code);
+    });
+    return;
+  }
+
   inputFiles.forEach((it) => {
     const content = fs.readFileSync(it, { encoding: 'utf-8' });
     const tokenStream = getTokenStream(content);
--- src/parsing.ts
+++ src/parsing.ts
@@ -5,6 +5,7 @@ import {
   ParserRuleContext,
 } from 'antlr4ts';
 import { AstNode } from './ast/astNode';
+import { DynastyCompiler } from './compile/compiler';
 import { DynastyLangLexer } from './generated/DynastyLangLexer';
 import { DynastyLangParser } from './generated/DynastyLangParser';
 import { BuildAstVisitor } from './visitors/buildAstVisitor';
@@ -22,4 +23,9 @@ export function parse(tokenStream: TokenStream): ParserRuleContext {
 export function generateAst(tree: ParserRuleContext): AstNode {
   let visitor = new BuildAstVisitor();
   return visitor.visit(tree);
+}
+
+export function compile(ast: AstNode, file: string): string {
+  const compiler = new DynastyCompiler(ast, file);
+  return compiler.compile();
 }
