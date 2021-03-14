9e040c2cdea0483c47e90a1f44ac8b1cc9660ff0
frodo821 <sakaic2003@gmail.com>
2021-03-14 12:07:00 +0900

    型システムを途中まで実装

--- src/compile/compiler.ts
+++ src/compile/compiler.ts
@@ -3,7 +3,7 @@ import { NameCollectVisitor } from './nameCollectVisitor';
 import { Namespace } from './codegen/codeElement';
 import { AsmNode, generateAsm } from './codegen/assembly';
 import { AssemblyGenerator } from './codegen/templates';
-import { uint8 } from './types';
+import { TypeDesc, uint8 } from './types';
 
 export class DynastyCompiler {
   private rootNS: Namespace;
@@ -34,14 +34,86 @@ export class DynastyCompiler {
     };
   }
 
+  /**
+   * TODO: これを実装
+   */
   resolveImports() {}
-  resolveTypeReferences() {}
+
+  resolveTypeReferences() {
+    this.resolveTypes(this.rootNS, []);
+    console.log(JSON.stringify(this.rootNS, undefined, 2));
+  }
+
+  private resolveTypes(ns: Namespace, parents: Namespace[]) {
+    Object.entries(ns.types)
+      .filter(([_, it]) => it.kind === 'reference')
+      .forEach(([key, it]) => {
+        if (it.kind !== 'reference') {
+          return;
+        }
+
+        let knownRefs: string[] = [key];
+        let ref: TypeDesc = it;
+
+        while (true) {
+          if (ref.kind === 'reference') {
+            if (knownRefs.includes(ref.name)) {
+              throw new Error(
+                `TypeError: cannot resolve circular type alias: ${knownRefs.join(
+                  ', '
+                )}`
+              );
+            }
+            try {
+              ref = this.scanTypes(ref.name, [...parents, ns]);
+            } catch {
+              break;
+            }
+            continue;
+          }
+          knownRefs.forEach((ref_) => {
+            ns.types[ref_] = ref;
+          });
+        }
+
+        throw new Error(
+          `TypeError: cannot resolve type alias: ${knownRefs.join(', ')}`
+        );
+      });
+    const callables = Object.values(ns.callables);
+    callables.forEach(({ locals: it }) =>
+      this.resolveTypes(it, [...parents, it])
+    );
+    callables.forEach(({ type: it }) => {
+      it.return_type;
+    });
+  }
+
+  private scanTypes(name: string, ns: Namespace[]) {
+    for (let i = 0; i < ns.length; i++) {
+      const ref = ns[i].types[name];
+
+      if (typeof ref !== 'undefined') {
+        return ref;
+      }
+    }
+
+    throw new Error(`TypeError: cannot find type '${name}'`);
+  }
+
+  /**
+   * TODO: これを実装
+   */
   resolveUnknownTypes() {}
+
+  /**
+   * TODO: これを実装
+   */
   resolveFuncCalls() {}
 
   generateFuncCode() {
     this.generator.declareFunction({
-      name: '_toplevel_main',
+      name: 'main',
       nodes: this.generateTopLevelCode(),
       type: {
         kind: 'callable',
@@ -52,10 +124,16 @@ export class DynastyCompiler {
     });
   }
 
+  /**
+   * TODO: これを実装
+   */
   generateMetaCode(): AsmNode[] {
     return [];
   }
 
+  /**
+   * TODO: これを実装
+   */
   generateTopLevelCode(): AsmNode[] {
     return [];
   }
